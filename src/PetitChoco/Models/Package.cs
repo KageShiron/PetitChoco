2076605b7f86342399a1ec8ae76f6621977a5a24
KageShiron <kageshiron@gmail.com>
2017-05-27 01:59:21 +0900

    Add nuspec view

---
+++ src/PetitChoco/Models/Package.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using NuGet;
+
+namespace PetitChoco.Models
+{
+    public class Package : LocalPackage
+    {
+        public Uri PackageSourceUrl { get; set; }
+        public Uri ProjectSourceUrl { get; set; }
+        public Uri DocsUrl { get; set; }
+        public Uri MailingListUrl { get; set; }
+        public Uri BugTrackerUrl { get; set; }
+
+        public IEnumerable<string> Hoge
+        {
+            get => new[] {"a", "b", "c"};
+        }
+
+
+        public Package()
+        {
+            
+        }
+        public override Stream GetStream()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void ExtractContents(IFileSystem fileSystem, string extractPath)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override IEnumerable<IPackageFile> GetFilesBase()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override IEnumerable<IPackageAssemblyReference> GetAssemblyReferencesCore()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- src/PetitChoco/ViewModels/ViewModel.cs
+++ src/PetitChoco/ViewModels/ViewModel.cs
@@ -3,11 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using NuGet;
+using PetitChoco.Models;
+using PetitChoco.Nuspec;
+using Reactive.Bindings;
+using Reactive;
 
 namespace PetitChoco
 {
     class ViewModel
     {
-        
+
+        public ReactiveProperty<string> PackagePath { get; }
+        public ReactiveProperty<Package> Package { get; }
+
+        public ReactiveCommand LoadPackageCommand { get; }
+
+        public ViewModel()
+        {
+            PackagePath = new ReactiveProperty<string>();
+            Package =  new ReactiveProperty<Package>();
+            LoadPackageCommand = new ReactiveCommand();
+            LoadPackageCommand.Subscribe(() =>
+            {
+                MessageBox.Show("");
+                Package.Value = NuspecReader.Read(PackagePath.Value);
+            } );
+        }
     }
 }
